fee6dbb956e7028787ad729fe542fd9d72c87604
Roman Smirnov <smirnow3217831@gmail.com>
2026-03-24 13:13:43 +0300

    fix images

--- app/components/BeansSlider.vue
+++ app/components/BeansSlider.vue
@@ -1,184 +1,15 @@
-<!--<template>
-  <div class="beans-slider">
-    <div class="my_border">
-		<div class="flex w-full justify-between px-[15px] absolute">
-			<div class="w-[15%] p-[20px]">
-				<Hohloma/>
-			</div>
-			<div class="w-[15%] p-[20px]">
-				<Hohloma/>
-			</div>
-		</div>
-		<h1 class="slider-heading" :class="{ 'is-visible': isVisible }">Ювелирные Традиции России</h1>
-		<div class="img-holder">
-			<img :src="`${baseURL}images/girl.webp`" alt="image description">
-		</div>
-		<div class="flex w-full justify-between px-[15px] absolute bottom-0">
-			<div class="w-[20%]">
-				<Hohloma/>
-			</div>
-			<div class="w-[20%]">
-				<Hohloma/>
-			</div>
-		</div>
-    </div>
-  </div>
-</template>
-
-<script setup>
-const baseURL = useRuntimeConfig().app.baseURL
-
-const isVisible = ref(false);
-
-onMounted(() => {
-	setTimeout(() => {
-		isVisible.value = true;
-	}, 100);
-})
-</script>
-
-<style scoped>
-.beans-slider {
-	position: relative;
-	padding: 50px 0;
-}
-
-.my_border {
-	display: block;
-	position: relative;
-	z-index: 1;
-}
-
-.slider-heading {
-	font-size: 80px;
-	/*line-height: 170px;*/
-	letter-spacing: 9px;
-	text-align: center;
-	margin: 0;
-	opacity: 0;
-	transform: scale(0.5);
-	transition: all 0.6s ease 0.4s;
-	font-family: 'PoiretOne';
-	font-weight: 700;
-	padding: 0 48px;
-}
-
-.slider-heading.is-visible {
-	opacity: 1;
-	transform: scale(1);
-}
-
-.img-holder {
-	width: 440px;
-	margin: -15px auto 0;
-	position: relative;
-	z-index: 3;
-	opacity: 0;
-	transform: translateY(100px);
-	animation: slideUp 0.8s ease-out 1.8s forwards;
-}
-
-.img-holder img {
-	width: 100%;
-	height: auto;
-	display: block;
-}
-
-/* Верхняя граница - опускается сверху */
-.my_border:before {
-	content: '';
-	position: absolute;
-	left: 24px;
-	right: 28px;
-	top: 0;
-	padding: 60px 0;
-	border: 14px solid currentColor;
-	border-bottom: none;
-	z-index: 2;
-	opacity: 0;
-	transform: translateY(-100%);
-	animation: slideDown 0.6s ease-out 0.6s forwards;
-}
-
-/* Нижняя граница - поднимается снизу */
-.my_border:after {
-	content: '';
-	position: absolute;
-	left: 24px;
-	right: 28px;
-	bottom: 0px;
-	padding: 260px 0;
-	border: 14px solid currentColor;
-	border-top: none;
-	z-index: 2;
-	opacity: 0;
-	transform: translateY(100%);
-	animation: slideUp 0.6s ease-out 1.2s forwards;
-}
-
-/* Анимации */
-@keyframes fadeIn {
-	to {
-		opacity: 1;
-	}
-}
-
-@keyframes slideDown {
-	to {
-		opacity: 1;
-		transform: translateY(0);
-	}
-}
-
-@keyframes slideUp {
-	to {
-		opacity: 1;
-		transform: translateY(0);
-	}
-}
-
-/* Адаптив */
-@media (max-width: 768px) {
-	.slider-heading {
-		font-size: 40px;
-		line-height: 50px;
-		letter-spacing: 4px;
-	}
-
-	.img-holder {
-		width: 90%;
-		max-width: 240px;
-		margin-top: 0px;
-	}
-
-	.my_border:after {
-		padding: 20% 0;
-	}
-}
-</style>-->
-
-
-
 <template>
   <div class="beans-slider">
     <div class="my_border">
-		<!--<div class="flex w-full justify-between px-[15px] absolute corner-top">
-			<div class="corner-item">
-				<Hohloma/>
-			</div>
-			<div class="corner-item">
-				<Hohloma/>
-			</div>
-		</div>-->
 		<h1 class="slider-heading" :class="{ 'is-visible': isVisible }">Ювелирные Традиции России</h1>
 		<div class="img-holder">
 			<img :src="`${baseURL}images/girl.webp`" alt="image description">
 		</div>
 		<div class="flex w-full justify-between px-[15px] absolute bottom-0 corner-bottom">
-			<div class="corner-item">
+			<div class="corner-item" style="transform: rotate(45deg) scaleX(-1)">
 				<Hohloma/>
 			</div>
-			<div class="corner-item">
+			<div class="corner-item" style="transform: rotate(-45deg);">
 				<Hohloma/>
 			</div>
 		</div>
@@ -211,7 +42,7 @@ onMounted(() => {
 }
 
 .corner-item {
-	width: 15%;
+	width: 30%;
 	padding: 20px;
 }
 
@@ -247,6 +78,7 @@ onMounted(() => {
 	width: 100%;
 	height: auto;
 	display: block;
+	padding-bottom: 14px;
 }
 
 .my_border:before {
@@ -255,7 +87,7 @@ onMounted(() => {
 	left: 24px;
 	right: 28px;
 	top: 0;
-	padding: 60px 0;
+	padding: 70px 0;
 	border: 14px solid currentColor;
 	border-bottom: none;
 	z-index: 2;
@@ -270,7 +102,7 @@ onMounted(() => {
 	left: 24px;
 	right: 28px;
 	bottom: 0px;
-	padding: 260px 0;
+	padding: 270px 0;
 	border: 14px solid currentColor;
 	border-top: none;
 	z-index: 2;
@@ -307,7 +139,7 @@ onMounted(() => {
 	}
 
 	.corner-item {
-		width: 18%;
+		width: 35%;
 		padding: 15px;
 	}
 
@@ -333,13 +165,17 @@ onMounted(() => {
 		font-size: 32px;
 		line-height: 40px;
 		letter-spacing: 3px;
-		padding: 0 24px;
+		padding: 24px;
 	}
 
 	.img-holder {
-		width: 85%;
-		max-width: 280px;
+		width: 100%;
+		max-width: 380px;
 		margin-top: 30px;
+	}
+
+	.img-holder img {
+		padding-bottom: 8px;
 	}
 
 	.corner-top {
@@ -351,7 +187,7 @@ onMounted(() => {
 	}
 
 	.corner-item {
-		width: 20%;
+		width: 40%;
 		padding: 8px;
 	}
 
@@ -363,11 +199,11 @@ onMounted(() => {
 	}
 
 	.my_border:before {
-		padding: 40px 0;
+		padding: 60px 0;
 	}
 
 	.my_border:after {
-		padding: 25% 0;
+		padding: 35% 0;
 	}
 }
 
@@ -377,22 +213,26 @@ onMounted(() => {
 		font-size: 24px;
 		line-height: 32px;
 		letter-spacing: 2px;
-		padding: 0 16px;
+		padding: 24px 16px;
 	}
 
 	.img-holder {
-		width: 90%;
-		max-width: 150px;
+		width: 100%;
+		max-width: 250px;
 		margin-top: 25px;
 	}
 
+	.img-holder img {
+		padding-bottom: 5px;
+	}
+
 	.corner-item {
-		width: 22%;
+		width: 40%;
 		padding: 6px;
 	}
 
 	.my_border:before {
-		padding: 30px 0;
+		padding: 90px 0;
 	}
 
 	.my_border:after {
